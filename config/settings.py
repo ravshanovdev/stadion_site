96661ecc46e6defa020b3fdae3d4b5ecd31ec7eb
sauron <kravshanov97@gmail.com>
2025-04-06 16:23:43 +0500

    added many changes and i can't use webhook

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,9 @@
-
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.core.exceptions import ImproperlyConfigured
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -10,12 +11,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!(i(&d1@a@u=+w=b19_lchp!u+3luxn!g!uejj2#0s2v(wp@r'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .env faylini yuklab olish
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=0)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -135,8 +145,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
-STRIPE_SECRET_KEY = 'sk_test_51OAVtyBA9ggbbHfPjVU7N9rsllDdjrayyiEB0qlds9USm1zL56GywYprFojFv5H54J4iBbhW4IuWcSIyKrO2DYpD009cG91J3i'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_51OAVtyBA9ggbbHfPmQZuCw3cyYKty0TN3A3vif6TlfA3TYjUDDiMXNR51VCNGo2YjXWtKAjtChxosqye5TUYMsHF00x4ieWW4L'
-STRIPE_WEBHOOK_SECRET = 'whsec_7cbfe5a2c68adbd4b4774a2d4658c594674d0b3bb2cb5ed7025dccd5b4d41b77'
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
 
 
